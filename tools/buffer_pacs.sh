5d8bb92dff58b44229cc69d12403e3b63f686064
william <liyijiang3000@163.com>
2023-06-24 23:41:20 -0400

    fix bug

--- tools/buffer_pacs.sh
+++ tools/buffer_pacs.sh
@@ -2,7 +2,8 @@ save_path=work_dirs/PACS_ResNet18_experts50_50e
 now=$(date +"%Y%m%d_%H%M%S")
 mkdir -p $save_path
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python buffer.py \
+CUDA_VISIBLE_DEVICES=6 python buffer.py \
+    --batch_train 64 --batch_real 64 \
     --dataset=PACS --model=ResNet18_ImageNet \
     --train_epochs=50 --num_experts=50 \
     --buffer_path=work_dirs/PACS_ResNet18_experts50_50e \
